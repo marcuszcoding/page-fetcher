6805d8eac92c10462851be1edc0f95e4d519ab62
marcuszcoding <marcuszcoding@gmail.com>
2022-12-12 13:05:39 -0700

    added in error if statement while learning about writing a new file

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,13 @@
+const fs = require('fs')
 const URL = process.argv[2]
 const filePath = process.argv[3]
 const request = require('request');
 
 
-request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
+request(URL, (error, response, body) => {
+  if (error) {
   console.log('error:', error); // Print the error if one occurred
+  }
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
@@ -17,4 +20,5 @@ request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
  a.) going to npm init and npm install request!
 3.) After our response we need to a new file to save our information from our request in
  a.) how do we put things into a local file? lets find out
-*/
+4.) Add in your URL variable to the old request function, add an if for error?
+ */
